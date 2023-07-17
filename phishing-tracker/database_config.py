cf6707b0beb939cd0d48076fc3ddbfd0673218e6
Emre Can Seçer <103491438+seceremrecan@users.noreply.github.com>
2023-07-17 18:07:20 +0300

    Update database_config.py

--- phishing-tracker/database_config.py
+++ phishing-tracker/database_config.py
@@ -1,7 +1,10 @@
 import os
+import csv
 import configparser
 import logging
 import requests
+from requests import Session
+
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,7 +12,7 @@ from sqlalchemy.orm import Session
 from models import Base
 
 config = configparser.ConfigParser()
-config.read(".env")
+config.read("/home/emre/Desktop/phishing-tracker/.env")
 
 db_host = os.getenv("DB_HOST", config["database"]["host"])
 db_user = os.getenv("DB_USER", config["database"]["user"])
@@ -20,6 +23,7 @@ DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+session = Session()
 
 
 def fetch_url_data(url):
@@ -48,11 +52,16 @@ def save_urls_to_db(db: Session, urls, model):
         urls (list): URLs to be updated.
         model (Base): Database model.
     """
-    existing_urls = db.query(model).all()
+    existing_urls = db.query(model).filter(model.url.in_(urls)).all()
     existing_url_set = set(url.url for url in existing_urls)
-    new_urls = [url for url in urls if url not in existing_url_set]
-    db.bulk_save_objects([model(url=url) for url in new_urls])
-    db.commit()
+    new_urls = list(set(urls) - existing_url_set)
+    
+    if new_urls:
+        db.bulk_save_objects([model(url=url) for url in new_urls])
+        db.commit()
+
+
+
 
 
 def fetch_and_save_urls(db: Session, url, model, success_message):
@@ -73,15 +82,37 @@ def fetch_and_save_urls(db: Session, url, model, success_message):
     except Exception as e:
         logging.error(f"Error: {str(e)}")
 
-def fetch_and_save_urls_json(db: Session, url: str, model: Base, success_message: str):
+     
+def fetch_and_save_urls_csv(db: Session, url: str, model: Base, success_message: str):
     try:
         response = requests.get(url)
         response.raise_for_status()
 
-        data = response.json()
-        urls = [entry['url'] for entry in data]
-
+        csv_data = response.text.split("\n")
+        rows = [row.split(",") for row in csv_data[1:] if row]
+        urls = [row[1] for row in rows]
         save_urls_to_db(db, urls, model)
         logging.info(success_message)
     except Exception as e:
-        logging.error(f"Error: {str(e)}")        
+        logging.error(f"Error: {str(e)}")
+
+
+
+def fetch_and_save_urls_csv_phishstats(db: Session, url: str, model: Base, success_message: str):
+    response = requests.get(url)
+    csv_data = response.text.strip().split('\n')[6:]  # Header'ı ve gereksiz satırları atlıyoruz
+
+    urls = []
+
+    # CSV içeriğini döngüyle oku ve sadece http ile başlayan URL'leri al
+    reader = csv.reader(csv_data, delimiter=',', quotechar='"')
+    for row in reader:
+        if len(row) >= 3:
+            url = row[2].strip()
+            if url.startswith('http'):
+                urls.append(url)
+
+    #print(urls)
+    save_urls_to_db(db, urls, model)  # Veritabanına kaydetme işlemi
+
+    print(success_message)
